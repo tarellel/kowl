28399e3963719803db58e49bf56f0bcbbe43c49a
Brandon Hicks <tarellel@gmail.com>
2021-04-18 00:02:02 -0600

    CHANGED: Bumped ruby version for mimemagic dependency issue
    * Bumped rails version for mimemagic
    * Converted require to require_relative

--- lib/kowl.rb
+++ lib/kowl.rb
@@ -12,7 +12,7 @@ require 'kowl/generators/overrides/app_generator'
 # Load the list of customized application generators
 generators = %w[action admin assets config controller database decorators docker dotfiles libs mailer misc pages routes
                 sidekiq staging test text_files users_and_auth uuid views_and_helpers]
-generators.map { |file| require "kowl/generators/#{file}_generator" }
+generators.map { |file| require_relative "kowl/generators/#{file}_generator" }
 
 # For generating base template files, once the application has been build
 require 'kowl/generators/overrides/app_builder'
